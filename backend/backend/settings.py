22a42892c1f879ccb30c04ac59ad35c35ea9d4de
Aref Abedjooy <arefabedjooy@gmail.com>
2023-01-03 18:43:34 -0500

    Add rest_framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',  # Add this
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
